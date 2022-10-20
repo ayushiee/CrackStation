a95a409f8942b29b26564e8cb142d649f1750dc2
Ayushi Gupta <ayushigupta8@gmail.com>
2022-10-20 16:39:39 -0700

    feat: added tests

--- Tests/W5CrackStationTests/W5CrackStationTests.swift
+++ Tests/W5CrackStationTests/W5CrackStationTests.swift
@@ -2,10 +2,28 @@ import XCTest
 @testable import W5CrackStation
 
 final class W5CrackStationTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(W5CrackStation().text, "Hello, World!")
+
+    /// Test to check hash string exists and matches a single decrypted alphabet.
+    func testIsDecryptable() throws {
+        let mockInput = "95cb0bfd2977c761298d9624e4b4d4c72a39974a"
+        let expectedAns = "y"
+        
+        let crackStation = W5CrackStation()
+        let response = try crackStation.decryptPassword(from: mockInput)
+        
+        XCTAssertNotNil(response)
+        XCTAssert(response == expectedAns)
+    }
+
+    /// Test to check if package fails when incorrect hash is entered for a given alphabet
+    func testIsNotDecryptable() throws {
+        let mockInput = "95cb0bfd2977c761298d9624e4b4d4c72a39974b"
+        let expectedAns = "y"
+        
+        let crackStation = W5CrackStation()
+        let response = try crackStation.decryptPassword(from: mockInput)
+        
+        XCTAssertNotNil(response)
+        XCTAssertFalse(response == expectedAns)
     }
 }
