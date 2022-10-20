4b4622105e9afd0bde9fed176b3a982839bd0598
Ayushi Gupta <ayushigupta8@gmail.com>
2022-10-20 16:35:06 -0700

    feat: added encrypt/decrypt functions

--- Sources/W5CrackStation/W5CrackStation.swift
+++ Sources/W5CrackStation/W5CrackStation.swift
@@ -1,6 +1,46 @@
-public struct W5CrackStation {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import CryptoKit
 
+@available(macOS 10.15, *)
+public struct W5CrackStation {
     public init() {
+    }
+    
+    /// Input: a string.
+    /// Output: the string encrypted using the SHA-1 algorithm.
+    func encryptUsingSha1(from input: String) -> String {
+        let inputData = Data(input.utf8)
+        let output = Insecure.SHA1.hash(data: inputData)
+        let result = output.description
+        let str = result.range(of: "SHA1 digest:")
+        return result[str!.upperBound...].trimmingCharacters(in: .whitespaces)
+    }
+    
+    /// Generates a SHA1 hash lookup dict for [a-zA-Z0-9]
+    func generateHashDict() -> [String: String]{
+        var hashDict: [String: String] = [:]
+        
+        for value in UnicodeScalar("a").value...UnicodeScalar("z").value {
+            let char = String(UnicodeScalar(value)!)
+            hashDict[encryptUsingSha1(from: char)] = char
+        }
+        
+        for value in UnicodeScalar("A").value...UnicodeScalar("Z").value {
+            let char = String(UnicodeScalar(value)!)
+            hashDict[encryptUsingSha1(from: char)] = char
+        }
+        
+        for value in UnicodeScalar("0").value...UnicodeScalar("1").value {
+            let char = String(UnicodeScalar(value)!)
+            hashDict[encryptUsingSha1(from: char)] = char
+        }
+        
+        return hashDict
+    }
+    
+    /// Decrypt the passed hash to corresponding str
+    func decryptPassword(from input: String) throws -> String?{
+        let hash = generateHashDict()
+        return String(hash[input] ?? "Incorrect response")
     }
 }
