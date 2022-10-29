9660797e4e94a662b37e1842bf8739095e051bd2
Ayushi Gupta <ayushigupta8@gmail.com>
2022-10-28 18:23:39 -0700

    update: generate hash of length 2

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -5,12 +5,13 @@ enum Hash {
     case SHA1, SHA256
 }
 
-
 @available(macOS 10.15, *)
 public struct CrackStation: Decrypter {
     public init() {
     }
     
+    /// Input: string, hashing algorithm
+    /// Output: corresponding hash
     func encrypt(from input: String, hash: Hash) -> String {
         let inputData = Data(input.utf8)
         let cacheStr: String
@@ -29,50 +30,34 @@ public struct CrackStation: Decrypter {
         
         let str = result.range(of: cacheStr)
         return result[str!.upperBound...].trimmingCharacters(in: .whitespaces)
-        
     }
     
-    /// Input: a string.
-    /// Output: the string encrypted using the SHA-1 algorithm.
-    func encryptUsingSha1(from input: String) -> String {
-        let inputData = Data(input.utf8)
-        let output = Insecure.SHA1.hash(data: inputData)
-        let result = output.description
-        let str = result.range(of: "SHA1 digest:")
-        return result[str!.upperBound...].trimmingCharacters(in: .whitespaces)
-    }
-    
-    func encryptUsingSha2(from input: String) -> String {
-        let inputData = Data(input.utf8)
-        let output256 = SHA256.hash(data: inputData)
-        let result = output256.description
-        let str = result.range(of: "SHA256 digest:")
-        return result[str!.upperBound...].trimmingCharacters(in: .whitespaces)
-    }
     
     /// Generates a SHA1 hash lookup dict for [a-zA-Z0-9]
     func generateHashDict() -> [String: String]{
         var hashDict: [String: String] = [:]
         
-        for value in UnicodeScalar("a").value...UnicodeScalar("z").value {
-            let char = String(UnicodeScalar(value)!)
-            print("#####: ", encrypt(from: char, hash: Hash.SHA1))
-            hashDict[encryptUsingSha1(from: char)] = char
-//            hashDict[encryptUsingSha2(from: char)] = char
-        }
-        
-        for value in UnicodeScalar("A").value...UnicodeScalar("Z").value {
-            let char = String(UnicodeScalar(value)!)
-            hashDict[encryptUsingSha1(from: char)] = char
-            hashDict[encryptUsingSha2(from: char)] = char
-        }
-        
-        for value in UnicodeScalar("0").value...UnicodeScalar("9").value {
-            let char = String(UnicodeScalar(value)!)
-            hashDict[encryptUsingSha1(from: char)] = char
-            hashDict[encryptUsingSha2(from: char)] = char
-        }
+        let possibleChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+        for letter in possibleChar {
+            // Generating strings of length 1
+            var generatedStr: String = ""
+            generatedStr.append(letter)
+            
+            hashDict[encrypt(from: generatedStr, hash: Hash.SHA1)] = generatedStr
+            hashDict[encrypt(from: generatedStr, hash: Hash.SHA256)] = generatedStr
+            
+            for secondLetter in possibleChar {
+                // Generating strings of length 2
+                var generatedStr: String = ""
+                generatedStr.append(letter)
+                generatedStr.append(secondLetter)
+                
+                hashDict[encrypt(from: generatedStr, hash: Hash.SHA1)] = generatedStr
+                hashDict[encrypt(from: generatedStr, hash: Hash.SHA256)] = generatedStr
+            }
+        }
+        
         return hashDict
     }
     
