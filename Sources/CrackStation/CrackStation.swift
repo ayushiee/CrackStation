35d13b29dab5b5bbfbbd36fef440b596307754d4
Ayushi Gupta <ayushigupta8@gmail.com>
2022-11-16 11:33:32 -0800

    feat: added json generator code

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -6,11 +6,14 @@ enum Hash {
 }
 
 public class CrackStation: Decrypter {
-    var hashDict: [String: String] = [:]
+    private var hashDict: [String: String] = [:]
     
     required public init() {
         do {
             hashDict = try loadDictionaryFromDisk()
+            if hashDict.isEmpty {
+                print("Lookup table dictionary does not exist. Please generate that first!")
+            }
         } catch {
             print("Error: ", error)
         }
@@ -29,57 +32,6 @@ public class CrackStation: Decrypter {
         }
     }
 
-    /// Input: string, hashing algorithm
-    /// Output: corresponding hash
-    func encrypt(from input: String, hash: Hash) -> String {
-        let inputData = Data(input.utf8)
-        let cacheStr: String
-        let result: String
-        
-        switch(hash) {
-        case .SHA1:
-            let output = Insecure.SHA1.hash(data: inputData)
-            cacheStr = "SHA1 digest:"
-            result = output.description
-        case .SHA256:
-            let output256 = SHA256.hash(data: inputData)
-            cacheStr = "SHA256 digest:"
-            result = output256.description
-        }
-        
-        let str = result.range(of: cacheStr)
-        return result[str!.upperBound...].trimmingCharacters(in: .whitespaces)
-    }
-    
-    
-    /// Generates a SHA1 hash lookup dict for [a-zA-Z0-9]
-    func generateHashDict() -> [String: String]{
-        var hashDict: [String: String] = [:]
-        
-        let possibleChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-        for letter in possibleChar {
-            // Generating strings of length 1
-            var generatedStr: String = ""
-            generatedStr.append(letter)
-            
-            hashDict[encrypt(from: generatedStr, hash: Hash.SHA1)] = generatedStr
-            hashDict[encrypt(from: generatedStr, hash: Hash.SHA256)] = generatedStr
-            
-            for secondLetter in possibleChar {
-                // Generating strings of length 2
-                var generatedStr: String = ""
-                generatedStr.append(letter)
-                generatedStr.append(secondLetter)
-                
-                hashDict[encrypt(from: generatedStr, hash: Hash.SHA1)] = generatedStr
-                hashDict[encrypt(from: generatedStr, hash: Hash.SHA256)] = generatedStr
-            }
-        }
-        
-        return hashDict
-    }
-    
     /// Decrypt the passed hash to corresponding alphabet string
     public func decrypt(shaHash: String) -> String?{
         return hashDict[shaHash]
